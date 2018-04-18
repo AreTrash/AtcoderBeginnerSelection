b0cbb0fdbd96f6929d8d3e49c4f95ea614eec37d
AreTrash <tinpuny44@gmail.com>
2018-04-19 04:51:22 +0900

    feat: practiceA

---
+++ PracticeA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PracticeA
+{
+    public class Program
+    {
+        void Solve(StreamScanner ss, StreamWriter sw)
+        {
+            //---------------------------------
+            var A = ss.Next(int.Parse);
+            var B = ss.Next(int.Parse);
+            var C = ss.Next(int.Parse);
+            var S = ss.Next(String);
+
+            sw.WriteLine($"{A + B + C} {S}");
+            //---------------------------------
+        }
+
+        static void Main()
+        {
+            var ss = new StreamScanner(new StreamReader(Console.OpenStandardInput()));
+            var sw = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            new Program().Solve(ss, sw);
+            sw.Flush();
+        }
+
+        static readonly Func<string, string> String = s => s;
+    }
+
+    public class StreamScanner
+    {
+        static readonly char[] Sep = {' '};
+        readonly Queue<string> buffer = new Queue<string>();
+        readonly TextReader textReader;
+
+        public StreamScanner(TextReader textReader)
+        {
+            this.textReader = textReader;
+        }
+
+        public T Next<T>(Func<string, T> parser)
+        {
+            if (buffer.Count != 0) return parser(buffer.Dequeue());
+            var nextStrings = textReader.ReadLine().Split(Sep, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var nextString in nextStrings) buffer.Enqueue(nextString);
+            return Next(parser);
+        }
+    }
+}
