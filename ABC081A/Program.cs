a64d652e69c1f4b07688064aaedbd27132e1d0fe
AreTrash <tinpuny44@gmail.com>
2018-04-19 04:44:37 +0900

    feat: ABC081A

---
+++ ABC081A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace AtcoderBeginnerSelection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Count(c => c == '1'));
+        }
+    }
+}
