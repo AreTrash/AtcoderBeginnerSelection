e9c543f8d3e2c69558e5f4fdf19f11628c152d4f
AreTrash <tinpuny44@gmail.com>
2018-04-19 04:24:40 +0900

    feat: ABC083B

---
+++ ABC083B/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC083B
+{
+    public class Program
+    {
+        void Solve(StreamScanner ss, StreamWriter sw)
+        {
+            //---------------------------------
+            var N = ss.Next(int.Parse);
+            var A = ss.Next(int.Parse);
+            var B = ss.Next(int.Parse);
+
+            var ans = Enumerable.Range(1, N).Where(x =>
+            {
+                var sum = x.ToString().Sum(c => c - '0');
+                return A <= sum && sum <= B;
+            }).Sum();
+
+            sw.WriteLine(ans);
+            //---------------------------------
+        }
+
+        static void Main()
+        {
+            var ss = new StreamScanner(new StreamReader(Console.OpenStandardInput()));
+            var sw = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            new Program().Solve(ss, sw);
+            sw.Flush();
+        }
+
+        static readonly Func<string, string> String = s => s;
+    }
+
+    public class StreamScanner
+    {
+        static readonly char[] Sep = {' '};
+        readonly Queue<string> buffer = new Queue<string>();
+        readonly TextReader textReader;
+
+        public StreamScanner(TextReader textReader)
+        {
+            this.textReader = textReader;
+        }
+
+        public T Next<T>(Func<string, T> parser)
+        {
+            if (buffer.Count != 0) return parser(buffer.Dequeue());
+            var nextStrings = textReader.ReadLine().Split(Sep, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var nextString in nextStrings) buffer.Enqueue(nextString);
+            return Next(parser);
+        }
+    }
+}
