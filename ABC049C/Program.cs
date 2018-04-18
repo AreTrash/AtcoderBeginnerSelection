9c9490fb98d1ac8734841ebfb0ef1be0e34fceb3
AreTrash <tinpuny44@gmail.com>
2018-04-19 03:51:46 +0900

    feat: ABC049C

---
+++ ABC049C/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ABC049C
+{
+    public class Program
+    {
+        static readonly IEnumerable<string> Words = new[] {"dream", "dreamer", "erase", "eraser"};
+        string S;
+
+        void Solve(StreamScanner ss, StreamWriter sw)
+        {
+            //---------------------------------
+            S = ss.Next(s => s);
+            sw.WriteLine(Dfs(0) ? "YES" : "NO");
+            //---------------------------------
+        }
+
+        bool Dfs(int index)
+        {
+            if (index == S.Length) return true;
+
+            var ret = false;
+            foreach (var word in Words)
+            {
+                if (index + word.Length > S.Length || S.Substring(index, word.Length) != word) continue;
+                ret |= Dfs(index + word.Length);
+            }
+            return ret;
+        }
+
+        static void Main()
+        {
+            var ss = new StreamScanner(new StreamReader(Console.OpenStandardInput()));
+            var sw = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            new Program().Solve(ss, sw);
+            sw.Flush();
+        }
+    }
+
+    public class StreamScanner
+    {
+        static readonly char[] Sep = {' '};
+        readonly Queue<string> buffer = new Queue<string>();
+        readonly TextReader textReader;
+
+        public StreamScanner(TextReader textReader)
+        {
+            this.textReader = textReader;
+        }
+
+        public T Next<T>(Func<string, T> parser)
+        {
+            if (buffer.Count != 0) return parser(buffer.Dequeue());
+            var nextStrings = textReader.ReadLine().Split(Sep, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var nextString in nextStrings) buffer.Enqueue(nextString);
+            return Next(parser);
+        }
+    }
+}
